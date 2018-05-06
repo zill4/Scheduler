3b6b881d179b0b8a90e213cec4fd6f068bc1d3f4
zill4 <justcrisp@outlook.com>
2018-05-05 19:11:32 -0700

    switching to windows

--- ng_code/ng5/src/app/app-routing.module.ts
+++ ng_code/ng5/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { 
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'about/:id',
     component: AboutComponent
+  },
+  {
+    path: 'login/:id',
+    component: LoginComponent
   }
 ];
 
